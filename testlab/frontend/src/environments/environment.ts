484b7ba5b1e0e1980bdf9b20945ce03e95b30b28
EdgarCCosta <esantiagorodrigues@birt.es>
2026-01-13 01:09:36 +0100

    Commit Final Environment

--- testlab/frontend/src/environments/environment.ts
+++ testlab/frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     // apiUrl: 'http://localhost/proyecto/testlab/testlab/backend/public/api'
-    apiUrl: 'https://testlabfinal-production.up.railway.app/api'
+    apiUrl: 'testlabfinal-production-30bc.up.railway.app/api'
 };
